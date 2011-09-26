7a5001a56e8a97c0089bd12ddb3c232415dacda1
Jaime Melis <j.melis@fdi.ucm.es>
2011-09-26 09:40:23 +0200

    Refactor JS code - substitute options.js with model.js - pick better variables (options -> fields) - encapsulate functions by views and fields

--- model.js
+++ model.js
@@ -1,60 +1,77 @@
 var TYPES = [ "cover", "list", "grid", "hover" ];
-var OPTIONS = [
-    "Title",
-    "Director",
-    "Writer",
-    "Actors",
-    "Plot",
-    "Poster",
-    "Runtime",
-    "Rating",
-    "Votes",
-    "IMDb Rating",
-    "Genre",
-    "Released",
-    "Year",
-    "Rated",
-    "ID"
-];
 
-function set_options(view, options) {
-    views = JSON.parse(localStorage.views);
-
-    if (typeof(views[view]) == "undefined") {
-        views[view] = new Object();
+var Fields = {
+    "get": function() {
+        return localStorage.fields.split(",");
+    },
+    "set": function(fields) {
+        localStorage.fields = fields;
+    },
+    "load_defaults": function() {
+        localStorage.removeItem("fields");
+        localStorage.fields = [
+            "IMDb Rating",
+            "Director",
+            "Actors",
+            "Plot",
+            "Title",
+            "Writer",
+            "Poster",
+            "Runtime",
+            "Rating",
+            "Votes",
+            "Genre",
+            "Released",
+            "Year",
+            "Rated",
+            "ID"
+        ];
     }
-
-    views[view] = options;
-
-    localStorage.views = JSON.stringify(views);
-    return options;
 }
 
-function get_options(view) {
-    views = JSON.parse(localStorage.views);
-    if (views === null) {
-        return null;
-    }
-    return views[view];
-}
+var Views = {
+    "set": function(view, fields) {
+        views = JSON.parse(localStorage.views);
 
-function is_checked(view, option) {
-    options = get_options(view);
-    return $.inArray(option, options) >= 0;
+        if (typeof(views[view]) == "undefined") {
+            views[view] = new Object();
+        }
+
+        views[view] = fields;
+
+        localStorage.views = JSON.stringify(views);
+        return fields;
+    },
+    "get": function(view) {
+        views = JSON.parse(localStorage.views);
+        if (views === null) {
+            return null;
+        }
+        return views[view];
+    },
+    "is_checked": function(view, field) {
+        fields = Views.get(view);
+        return $.inArray(field, fields) >= 0;
+    },
+    "load_defaults": function() {
+        localStorage.removeItem("views");
+        views = new Object();
+        views.cover = [ "IMDb Rating", "Director", "Actors", "Plot" ];
+        views.list  = [ "IMDb Rating", "Director", "Actors", "Plot" ];
+        views.grid  = [ "IMDb Rating", "Director", "Actors", "Plot" ];
+        views.hover = [ "IMDb Rating", "Director", "Actors" ];
+        localStorage.views = JSON.stringify(views);
+    }
 }
 
 function load_defaults() {
-    localStorage.removeItem("views");
-    views = new Object();
-    views.cover = [ "IMDb Rating", "Director", "Actors", "Plot" ];
-    views.list  = [ "IMDb Rating", "Director", "Actors", "Plot" ];
-    views.grid  = [ "IMDb Rating", "Director", "Actors", "Plot" ];
-    views.hover = [ "IMDb Rating", "Director", "Actors", "Plot" ];
-    localStorage.views = JSON.stringify(views);
+    Views.load_defaults();
+    Fields.load_defaults();
 }
 
-function options_bootstrap() {
-    if (typeof(localStorage.views) == "undefined") {
+function bootstrap() {
+    if (typeof(localStorage.views) == "undefined"
+            || typeof(localStorage.fields) == "undefined") {
         load_defaults();
     }
 }
